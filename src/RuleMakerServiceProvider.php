497b79c0e1d847e5e67de682eadcbfe6441d4cbd
AsteroidSnowsuit <nicolas.sc@outlook.com>
2020-04-23 21:38:48 -0400

    Add RuleMaker Service Provider

---
+++ src/RuleMakerServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AsteroidSnowsuit\RuleMaker;
+
+use Illuminate\Support\ServiceProvider;
+use AsteroidSnowsuit\RuleMaker\Migration\MigrationsLoader;
+
+class RuleMakerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
